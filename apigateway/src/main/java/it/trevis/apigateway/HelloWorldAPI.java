b05fe8f6d95e090b44713f483243aa7da2088de1
Trevisan Marco <3vis93@gmail.com>
2019-11-21 13:07:21 +0100

    add kafka producer

--- apigateway/src/main/java/it/trevis/apigateway/HelloWorldAPI.java
+++ apigateway/src/main/java/it/trevis/apigateway/HelloWorldAPI.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
+import java.util.Properties;
 
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
@@ -19,30 +20,66 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Path("/hello")
 public class HelloWorldAPI {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+	private final static String TOPIC = "test";
+	private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+//            "localhost:9092,localhost:9093,localhost:9094";
+
 	Jsonb jsonb = JsonbBuilder.create();
-	
-	@Context
-	private Request req;
-	
+
 	@Context
 	private HttpHeaders httpHeader;
-	
+
 	@Context
 	private HttpServletRequest httpRequest;
-	
+
+	private static Producer<Long, String> createProducer() {
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return new KafkaProducer<>(props);
+	}
+
+	static void runProducer(final int sendMessageCount) throws Exception {
+		final Producer<Long, String> producer = createProducer();
+		long time = System.currentTimeMillis();
+
+		try {
+			for (long index = time; index < time + sendMessageCount; index++) {
+				final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, index, "Hello Mom " + index);
+
+				RecordMetadata metadata = producer.send(record).get();
+
+				long elapsedTime = System.currentTimeMillis() - time;
+				logger.info("sent record(key={} value={}), metadata(partition={}, offset={}), time={}", record.key(),
+						record.value(), metadata.partition(), metadata.offset(), elapsedTime);
+			}
+		} finally {
+			producer.flush();
+			producer.close();
+		}
+	}
+
 	/*
-	 * Al momento non è conforme allo standard Rest
-	 * Le chiamate get devono essere "cachabili" e sempre uguali dato il medesimo input
-	 * Per test si utilizzerà comunque una chiamata get
-	 * Da girare poi in post per standard Rest
+	 * Al momento non è conforme allo standard Rest Le chiamate get devono essere
+	 * "cachabili" e sempre uguali dato il medesimo input Per test si utilizzerà
+	 * comunque una chiamata get Da girare poi in post per standard Rest
 	 */
 	@GET
 	@Path("/")
@@ -50,64 +87,49 @@ public class HelloWorldAPI {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response getAll() {
 		logger.debug("Chiamata a getAll()..");
-		logger.info("Request: {} and HTTP Header: {}", req.toString(), httpHeader.getRequestHeaders().toString());
-		try {
-			
-			BufferedReader reader = new BufferedReader(new InputStreamReader(httpRequest.getInputStream()));
-	        StringBuilder out = new StringBuilder();
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	            out.append(line);
-	        }
-	        logger.info("ROW: {}", out.toString());
-	        reader.close();
-			
-			logger.info("END");
-		} catch (IOException e) {
-			logger.error("Errore IO..", e);
-		}
+		logger.debug("HTTP Header: {}", httpHeader.getRequestHeaders().toString());
 		/*
-		 * Scrivere nel topic kafka associato al microservizio HelloWorldRS per le Request
-		 * Il microservizio processa la Request e va in db postgresql ad estrarre i dati
-		 * I dati estratti vengono messi nel topic kafka di Response
-		 * Poll sul topic kafka di Res e quando ho i dati li ritorno come response json al chiamante
+		 * Scrivere nel topic kafka associato al microservizio HelloWorldRS per le
+		 * Request Il microservizio processa la Request e va in db postgresql ad
+		 * estrarre i dati I dati estratti vengono messi nel topic kafka di Response
+		 * Poll sul topic kafka di Res e quando ho i dati li ritorno come response json
+		 * al chiamante
 		 */
-		
-		return Response.ok(req, MediaType.APPLICATION_JSON).build();
+
+		return Response.ok(jsonb.toJson("risposta"), MediaType.APPLICATION_JSON).build();
 //		List<Integer> result = null;
 //		return Response.ok(jsonb.toJson(result), MediaType.APPLICATION_JSON).build();
 	}
-	
+
 	@POST
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response postAll() {
-		logger.debug("Chiamata a postAll()..");
-		logger.info("Request: {} and HTTP Header: {}", req.toString(), httpHeader.getRequestHeaders().toString());
+		logger.info("Chiamata a postAll()..");
+		logger.debug("HTTP Header: {}", httpHeader.getRequestHeaders().toString());
 		try {
-			
 			BufferedReader reader = new BufferedReader(new InputStreamReader(httpRequest.getInputStream()));
-	        StringBuilder out = new StringBuilder();
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	            out.append(line);
-	        }
-	        logger.info("ROW: {}", out.toString());
-	        reader.close();
-			
-			logger.info("END");
+			StringBuilder out = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				out.append(line);
+			}
+			logger.debug("ROW: {}", out.toString());
+			reader.close();
+			logger.debug("END");
 		} catch (IOException e) {
 			logger.error("Errore IO..", e);
 		}
 		/*
-		 * Scrivere nel topic kafka associato al microservizio HelloWorldRS per le Request
-		 * Il microservizio processa la Request e va in db postgresql ad estrarre i dati
-		 * I dati estratti vengono messi nel topic kafka di Response
-		 * Poll sul topic kafka di Res e quando ho i dati li ritorno come response json al chiamante
+		 * Scrivere nel topic kafka associato al microservizio HelloWorldRS per le
+		 * Request Il microservizio processa la Request e va in db postgresql ad
+		 * estrarre i dati I dati estratti vengono messi nel topic kafka di Response
+		 * Poll sul topic kafka di Res e quando ho i dati li ritorno come response json
+		 * al chiamante
 		 */
-		
+
 		return Response.ok(jsonb.toJson("risposta"), MediaType.APPLICATION_JSON).build();
 	}
-	
+
 }
