124908454226bed51d70f34d97b877257e715e28
Trevisan Marco <3vis93@gmail.com>
2019-11-22 16:58:33 +0100

    alcuni cambiamenti

--- apigateway/src/main/java/it/trevis/apigateway/HelloWorldAPI.java
+++ apigateway/src/main/java/it/trevis/apigateway/HelloWorldAPI.java
@@ -18,10 +18,12 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -47,6 +49,8 @@ public class HelloWorldAPI {
 //            "localhost:9092,localhost:9093,localhost:9094";
 
 	Jsonb jsonb = JsonbBuilder.create();
+	
+	Base64.Encoder enc = Base64.getEncoder();
 
 	@Context
 	private Request req;
@@ -59,6 +63,15 @@ public class HelloWorldAPI {
 
 	@Context
 	private HttpServletRequest httpRequest;
+	
+	private static Producer<Long, Map<String,Object>> createMyProducer() {
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return new KafkaProducer<>(props);
+	}
 
 	private static Producer<Long, String> createProducer() {
 		Properties props = new Properties();
@@ -69,6 +82,21 @@ public class HelloWorldAPI {
 		return new KafkaProducer<>(props);
 	}
 
+	static void runMyProducer(Map<String, Object> mapToTopic) throws Exception {
+		final Producer<Long, Map<String, Object>> producer = createMyProducer();
+		long time = System.currentTimeMillis();
+
+		try {
+			final ProducerRecord<Long, Map<String, Object>> record = new ProducerRecord<>(TOPIC, time, mapToTopic);
+			RecordMetadata metadata = producer.send(record).get();
+			logger.info("sent record(key={} value={}), metadata(partition={}, offset={}), time={}", record.key(),
+					record.value(), metadata.partition(), metadata.offset(), time);
+		} finally {
+			producer.flush();
+			producer.close();
+		}
+	}
+	
 	static void runProducer(String message) throws Exception {
 		final Producer<Long, String> producer = createProducer();
 		long time = System.currentTimeMillis();
@@ -90,11 +118,11 @@ public class HelloWorldAPI {
 	 * comunque una chiamata get Da girare poi in post per standard Rest
 	 */
 	@GET
-	@Path("/")
+	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getAll() {
-		logger.debug("Chiamata a getAll()..");
+	public Response getAll(@PathParam("id") String id) {
+		logger.info("Chiamata a getAll()..");
 		logger.info("Request: {}", req.getMethod().toString());
 		logger.info("URI info: {}", uriInfo.getPath());
 		logger.debug("HTTP Header: {}", httpHeader.getRequestHeaders().toString());
@@ -105,6 +133,16 @@ public class HelloWorldAPI {
 		 * Poll sul topic kafka di Res e quando ho i dati li ritorno come response json
 		 * al chiamante
 		 */
+		/*
+		 * manca il GET da passare in kafka per dire che è una select
+		 * se passo DELETE con id diventa una DELETE per kafka
+		 */
+		try {
+			runProducer(id);
+		} catch (Exception e) {
+			logger.error("Errore runProducer..", e);
+		}
+		
 
 		return Response.ok(jsonb.toJson("risposta"), MediaType.APPLICATION_JSON).build();
 //		List<Integer> result = null;
@@ -123,7 +161,6 @@ public class HelloWorldAPI {
 		logger.debug("HTTP Header: {}", httpHeader.getRequestHeaders().toString());
 		try {
 //			ServletInputStream is = httpRequest.getInputStream();
-			Base64.Encoder enc = Base64.getEncoder();
 			
 			/*
 			 * creare un json con Method = POST
